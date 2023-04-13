b183cd1fa66731287a8edb0e86f3d39c6e69779e
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-04-13 12:04:26 -0300

    Adicionando classNames.

--- players/src/componentes/cabecalho/index.js
+++ players/src/componentes/cabecalho/index.js
@@ -1,13 +1,13 @@
 import './cabecalho.css'
 
 const Cabecalho = () => {
-    return (<section>
+    return (<section className="cabecalho">
         <ul>
-            <li><button><img src="./imagens/logos/street-hockey-brasil.png" alt="logo street hockey brasil" /></button></li>
-            <li><button><img src="./imagens/logos/etapa-outono.png" alt="logo etapa outono" /></button></li>
-            <li><button><img src="./imagens/logos/etapa-inverno.png" alt="logo etapa inverno" /></button></li>
-            <li><button><img src="./imagens/logos/etapa-primavera.png" alt="logo etapa primavera" /></button></li>
-            <li><button><img src="./imagens/logos/etapa-verao.png" alt="logo etapa verao" /></button></li>
+            <li><button className="btn"><img src="./imagens/logos/street-hockey-brasil.png" alt="logo street hockey brasil" /></button></li>
+            <li><button className="btn"><img src="./imagens/logos/etapa-outono.png" alt="logo etapa outono" /></button></li>
+            <li><button className="btn"><img src="./imagens/logos/etapa-inverno.png" alt="logo etapa inverno" /></button></li>
+            <li><button className="btn"><img src="./imagens/logos/etapa-primavera.png" alt="logo etapa primavera" /></button></li>
+            <li><button className="btn"><img src="./imagens/logos/etapa-verao.png" alt="logo etapa verao" /></button></li>
         </ul>
     </section>)
 }
