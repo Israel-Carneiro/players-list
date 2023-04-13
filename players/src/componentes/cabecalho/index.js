9d34babd3d9ee866958a51fab0c5f57a7724e064
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-04-13 12:02:24 -0300

    Importando logos para imagens em botões.

--- players/src/componentes/cabecalho/index.js
+++ players/src/componentes/cabecalho/index.js
@@ -3,11 +3,11 @@ import './cabecalho.css'
 const Cabecalho = () => {
     return (<section>
         <ul>
-            <li><button><img src="" alt="" /></button></li>
-            <li><button><img src="" alt="" /></button></li>
-            <li><button><img src="" alt="" /></button></li>
-            <li><button><img src="" alt="" /></button></li>
-            <li><button><img src="" alt="" /></button></li>
+            <li><button><img src="./imagens/logos/street-hockey-brasil.png" alt="logo street hockey brasil" /></button></li>
+            <li><button><img src="./imagens/logos/etapa-outono.png" alt="logo etapa outono" /></button></li>
+            <li><button><img src="./imagens/logos/etapa-inverno.png" alt="logo etapa inverno" /></button></li>
+            <li><button><img src="./imagens/logos/etapa-primavera.png" alt="logo etapa primavera" /></button></li>
+            <li><button><img src="./imagens/logos/etapa-verao.png" alt="logo etapa verao" /></button></li>
         </ul>
     </section>)
 }
