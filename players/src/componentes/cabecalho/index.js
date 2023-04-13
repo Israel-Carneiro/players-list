157b096538014f6e4d1ac02d19eb806ee12b601d
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-04-13 12:01:37 -0300

    Estrutura do componente Cabecalho.

---
+++ players/src/componentes/cabecalho/index.js
@@ -0,0 +1,15 @@
+import './cabecalho.css'
+
+const Cabecalho = () => {
+    return (<section>
+        <ul>
+            <li><button><img src="" alt="" /></button></li>
+            <li><button><img src="" alt="" /></button></li>
+            <li><button><img src="" alt="" /></button></li>
+            <li><button><img src="" alt="" /></button></li>
+            <li><button><img src="" alt="" /></button></li>
+        </ul>
+    </section>)
+}
+
+export default Cabecalho
