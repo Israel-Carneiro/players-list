954feee8d277944db7a7f3385a22d36d5f74990c
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-04-16 23:12:27 -0300

    Conteúdo do componente Card.

--- players/src/componentes/card/index.js
+++ players/src/componentes/card/index.js
@@ -1,10 +1,36 @@
 import './Card.css'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div>
-            <div></div>
-            <div></div>
+            <div>
+                <img src={props.imagem} alt={props.nome} />
+            </div>
+            <ul>
+                <li>
+                    <h2>{props.nome}</h2>
+                </li>
+                <div>
+                    <p>Rank</p>
+                    <p>{props.rank[indice]}</p>
+                </div>
+                <div>
+                    <p>Pontos</p>
+                    <p>{props.pontos[indice]}</p>
+                </div>
+                <div>
+                    <p>Gols</p>
+                    <p>{props.gols[indice]}</p>
+                </div>
+                <div>
+                    <p>Jogos</p>
+                    <p>{props.jogos[indice]}</p>
+                </div>
+                <div>
+                    <p>Rodadas</p>
+                    <p>{props.rodadas[indice]}</p>
+                </div>
+            </ul>
         </div>
     )
 }
