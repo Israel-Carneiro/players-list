077feef190b45acbe5fc782c9ff546251d6193b7
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-05-03 12:01:12 -0300

    Organizando em header.

--- players/src/componentes/cabecalho/index.js
+++ players/src/componentes/cabecalho/index.js
@@ -2,25 +2,36 @@ import Botao from '../Botao'
 import './Cabecalho.css'
 
 const Cabecalho = () => {
-    return (<section className="cabecalho">
+
+    return (<header className="cabecalho">
         <ul>
             <li>
-                <Botao><img src="./imagens/logos/street-hockey-brasil.png" alt="logo street hockey brasil" /></Botao>
+                <Botao>
+                    <img src="./imagens/logos/street-hockey-brasil.png" alt="logo street hockey brasil" />
+                </Botao>
             </li>
             <li>
-                <Botao><img src="./imagens/logos/etapa-outono.png" alt="logo etapa outono" /></Botao>
+                <Botao>
+                    <img src="./imagens/logos/etapa-outono.png" alt="logo etapa outono" />
+                </Botao>
             </li>
             <li>
-                <Botao><img src="./imagens/logos/etapa-inverno.png" alt="logo etapa inverno" /></Botao>
+                <Botao>
+                    <img src="./imagens/logos/etapa-inverno.png" alt="logo etapa inverno" />
+                </Botao>
             </li>
             <li>
-                <Botao><img src="./imagens/logos/etapa-primavera.png" alt="logo etapa primavera" /></Botao>
+                <Botao>
+                    <img src="./imagens/logos/etapa-primavera.png" alt="logo etapa primavera" />
+                </Botao>
             </li>
             <li>
-                <Botao><img src="./imagens/logos/etapa-verao.png" alt="logo etapa verao" /></Botao>
+                <Botao>
+                    <img src="./imagens/logos/etapa-verao.png" alt="logo etapa verao" />
+                </Botao>
             </li>
         </ul>
-    </section>)
+    </header>)
 }
 
 export default Cabecalho
